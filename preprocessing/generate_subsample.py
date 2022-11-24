7314090317e5c2477c104414cc66fe2e7168acac
Sebastian Celis <celis.sebastian@yahoo.com>
2022-11-24 16:25:22 +0100

    Add incident and place filtering

--- preprocessing/generate_subsample.py
+++ preprocessing/generate_subsample.py
@@ -3,34 +3,54 @@ import json
 import argparse
 
 
-def main():
-    # Read in arguments from Argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--pickle-file', type=str, default="../data/multi_label_train_v0.3.pkl",
-                        help='Path to input pickle file')
-    parser.add_argument('--output-file', type=str, default="../data/multi_label_train_v0.x.json",
-                        help='Path to output json file')
-    parser.add_argument('--num-samples', type=int, default=-1, help='Number of samples to take')
-    parser.add_argument('--ignore-unknown-classes', default=False,
-                        help='Ignore unknown classes', action='store_true')
-    args = parser.parse_args()
-
-    df = pd.read_pickle(args.pickle_file)
-    df = df[df["valid_image"] == True]
-    
-    if args.ignore_unknown_classes:
-        df = df[(df["incidents_list"] != "unknown") & df["places_list"] != "unknown"]
-
-    if args.num_samples > 0:
-        df = df.sample(args.num_samples, random_state=args.num_samples)
-    
+def save_df(df: pd.DataFrame, output_file: str):
     # Save df to json with line formatting
     values_dict = df[["incidents", "places"]].to_dict(orient="records")
     final_dict = {k: v for k, v in zip(df["key"], values_dict)}
 
     # Save final_dict to json
-    with open(args.output_file, "w") as f:
+    with open(output_file, "w") as f:
         json.dump(final_dict, f)
+
+
+def filter_df(df: pd.DataFrame, ignore_incidents: list[str], ignore_places: list[str]):
+    df = df[~df["incidents_list"].apply(lambda x: any([i in ignore_incidents for i in x.split(", ")]))].copy()
+    df = df[~df["places_list"].apply(lambda x: any([i in ignore_places for i in x.split(", ")]))].copy()
+
+    return df
+
+
+def main():
+    # Read in arguments from Argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--pickle-file', '-p', type=str, required=True, help='Path to input pickle file')
+    parser.add_argument('--output-file', '-o', type=str, default="./data/train.json",
+                        help='Path to output json file')
+    #parser.add_argument('--num-samples', type=int, default=-1, help='Number of samples to be included in the dataset')
+    parser.add_argument("--train-validation-split", '-s', type=float, default=1.0,
+                        help="Percentage of data to be used for training")
+    parser.add_argument("--ignore-incidents", '-ii', type=str, nargs="+",
+                        default=[], help="List of incidents to be ignored")
+    parser.add_argument("--ignore-places", '-ip', type=str, nargs="+", default=[], help="List of places to be ignored")
+    args = parser.parse_args()
+
+    print(f"Ignoring incidents: {args.ignore_incidents}")
+    print(f"Ignoring places: {args.ignore_places}")
+
+    df = pd.read_pickle(args.pickle_file)
+    df = df[df["valid_image"] == True].copy()
+
+    if args.train_validation_split < 1.0:
+        print("Train-validation split is set to {}.".format(args.train_validation_split))
+        print("The validation set will be saved in a separate file.")
+
+        val_df = df.sample(frac=1 - args.train_validation_split, random_state=42)
+        val_df = filter_df(val_df, args.ignore_incidents, args.ignore_places)
+        save_df(val_df, args.output_file.replace(".json", "_val.json"))
+        df = df.drop(val_df.index)
+
+    df = filter_df(df, args.ignore_incidents, args.ignore_places)
+    save_df(df, args.output_file)
 
 
 if __name__ == "__main__":
