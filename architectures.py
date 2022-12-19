029cf59a5842a2d4887f3f76afc0c6f8a2ea9798
Alessio Paone <32801591+Raffix-14@users.noreply.github.com>
2022-12-19 18:40:10 +0100

    Update architectures.py

--- architectures.py
+++ architectures.py
@@ -20,8 +20,8 @@ import tensorflow as tf
 
 # same loader used during training
 inference_loader = transforms.Compose([
-    transforms.Resize((256, 256)),
-    transforms.CenterCrop(224),
+    transforms.Resize((416, 416)),
+    transforms.CenterCrop(384),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
